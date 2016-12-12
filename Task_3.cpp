e7d566a466b369c17a63723d2003b0abf7ab3d64
Nikita Androsenko <varzaid@gmail.com>
2016-12-12 17:57:36 +0300

    Add files via upload

---
+++ Task_3.cpp
@@ -0,0 +1,70 @@
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>      
+#include <new>
+
+int q = 0, N = 0;
+void show_a();
+
+int **a;
+
+using namespace std;
+
+int main() {
+
+	setlocale(0, "");
+	do {
+		cout << "vvedite chislo <10" << endl;
+		cin >> N;
+	} while (N > 10);
+
+
+	a = new int *[N];   
+	for (int i = 0; i < N; i++) {
+		a[i] = new int[N];     
+	}
+
+	int n = N;
+
+	for (int i = 0; i < n;) {
+		for (int j = i; j < n; ++j) {
+			q++;
+			a[i][j] = q;
+
+		}
+		for (int j = i + 1; j < n; ++j) {
+			q++;
+			a[j][n - 1] = q;
+
+		}
+		--n;
+		for (int j = n - 1; j >= i; --j) {
+			q++;
+			a[n][j] = q;
+
+		}
+		++i;
+		for (int j = n - 1; j >= i; --j) {
+			q++;
+			a[j][i - 1] = q;
+
+		}
+	}
+
+	show_a();
+	system("pause");
+	return 0;
+}
+
+void show_a() {
+	for (int i = 0; i < N; i++) {
+		cout << endl;
+		for (int j = 0; j < N; j++) {
+			cout << setw(4) << a[i][j];
+		}
+		cout << endl;
+	}
+}
